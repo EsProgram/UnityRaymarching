398c06eb466847cb8fa5e90d5e758d94c0d2a7dd
EsProgram <radwimps.september@gmail.com>
2016-03-22 06:09:50 +0900

    SceneビューでのRaymarchingプレビュー機能を実装

--- Assets/Scripts/Raymarching/RaymarchingRenderer.cs
+++ Assets/Scripts/Raymarching/RaymarchingRenderer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -6,6 +8,23 @@ using UnityEngine.Rendering;
 public class RaymarchingRenderer : MonoBehaviour, IDisposable
 {
   private const CameraEvent RENDER_PASS = CameraEvent.AfterGBuffer;
+
+#if UNITY_EDITOR
+
+  /// SceneViewでのカメラ表示(複数シーンビュー対応)
+  /// PreviewSceneCameraをON/OFFにすることで切り替え可能
+  /// すぐには適用されないため、一度Playボタンを押す必要がある(用改善)
+  private const string USAGE =
+    @"シーンビューでRaymarchingを適用するかどうかを設定できます。
+    設定後は一度Playボタンを押してシーンビュー内のカメラを更新する必要があります。";
+
+  private static List<Camera> sceneCams = new List<Camera>();
+
+  [SerializeField, Tooltip(USAGE)]
+  private bool previewSceneCamera;
+
+#endif
+
   private Camera cam;
   private CommandBuffer command;
   private Mesh quad;
@@ -17,6 +36,14 @@ public class RaymarchingRenderer : MonoBehaviour, IDisposable
   {
     if(cam != null && command != null)
       cam.RemoveCommandBuffer(RENDER_PASS, command);
+#if UNITY_EDITOR
+    if(sceneCams.Count > 0 && command != null)
+      foreach(var scam in sceneCams)
+        if(scam != null)
+          scam.RemoveCommandBuffer(RENDER_PASS, command);
+    sceneCams.Clear();
+#endif
+
     cam = null;
     command = null;
   }
@@ -46,19 +73,25 @@ public class RaymarchingRenderer : MonoBehaviour, IDisposable
 
   private void OnPreRender()
   {
-    if(cam != null)
-      return;
+    if(cam == null)
+      cam = GetComponent<Camera>();
 
     if(quad == null)
       quad = CreateQuad();
 
-    cam = GetComponent<Camera>();
-
-    var com = new CommandBuffer();
-    com.name = "Raymarching";
-    com.DrawMesh(quad, Matrix4x4.identity, material, 0, 0);
-    cam.AddCommandBuffer(RENDER_PASS, com);
-
-    command = com;
+    if(command == null)
+    {
+      var com = new CommandBuffer();
+      com.name = "Raymarching";
+      com.DrawMesh(quad, Matrix4x4.identity, material, 0, 0);
+      cam.AddCommandBuffer(RENDER_PASS, com);
+#if UNITY_EDITOR
+      sceneCams = UnityEditor.SceneView.GetAllSceneCameras().ToList();
+      if(previewSceneCamera && sceneCams.Count > 0)
+        foreach(var scam in sceneCams)
+          scam.AddCommandBuffer(RENDER_PASS, com);
+#endif
+      command = com;
+    }
   }
 }
